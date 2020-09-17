1cb14df028672850648979028797f25d6523dadd
trevor-cuffe <trevor.d.cuffe@gmail.com>
2020-09-17 09:07:06 -0400

    Create app.js, set up routes, implement nodeJS, and convert landing page to EJS

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(express.static("images"));
+app.set("view engine", "ejs");
+
+//fix mongoose deprecation warnings:
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+//connect to Mongo db
+mongoose.connect('mongodb://localhost:27017/coffee_shop_app');
+
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+
+
+app.get("*", (req, res) => {
+    res.render("pageNotFound");
+});
+
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving coffeeShop on port ${port}`);
+})
